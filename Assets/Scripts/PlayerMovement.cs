1960f588a4ffcd975b5075e7c07dbf5ab69975d3
August Connolly <august.connolly@ucdconnect.ie>
2023-02-03 17:05:12 +0000

    Player Movement
    Very basic player movement implemented. Camera positioned to view full field.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 5f;
+    private Vector3 direction = new Vector3();
+    private CharacterController CC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        Vector3.Normalize(direction);
+        direction.y = -9f;
+        CC.Move(direction * speed * Time.deltaTime);
+    }
+}
